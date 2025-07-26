7e197c27dffb14e4ccd4772a8dea9f497f5bdf64
H A R S H A <harshavardhareddy92@gmail.com>
2025-07-26 10:54:08 +0530

    Add files via upload

---
+++ basic_problems/Q3.java
@@ -0,0 +1,11 @@
+// Q3: Widening conversion
+public class Q3 {
+    public static void main(String[] args) {
+        int a = 100;
+        long b = a;
+        float c = b;
+        System.out.println("Int: " + a);
+        System.out.println("Long: " + b);
+        System.out.println("Float: " + c);
+    }
+}
