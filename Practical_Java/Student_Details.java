f46ab3dd479732357a3d0e55807eac5f99fbac99
H A R S H A <harshavardhareddy92@gmail.com>
2025-07-26 10:26:40 +0530

    Add files via upload

---
+++ Practical_Java/Student_Details.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Student_Details {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter Student Name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter Roll Num: ");
+        int rollNo = scanner.nextInt();
+
+        System.out.print("Enter Age: ");
+        int age = scanner.nextInt();
+
+        System.out.print("Enter Year: ");
+        int year = scanner.nextInt();
+
+        System.out.println("\nStudent Details:");
+        System.out.println("Name: " + name);
+        System.out.println("Roll Number: " + rollNo);
+        System.out.println("Age: " + age);
+        System.out.println("Year: " + year);
+
+        scanner.close();
+    }
+}
