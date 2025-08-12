a12924c6f93a48c3fe8c7dde58fdefe4a9bb1de5
H A R S H A <harshavardhareddy92@gmail.com>
2025-08-12 12:31:26 +0530

    Add files via upload

---
+++ Scenario_based_Programms/MovieTicket.java
@@ -0,0 +1,42 @@
+class MovieTicket {
+    String movieName;
+    int ticketCount;
+    String seatType;
+    int pricePerTicket;
+
+    // Constructor with movie name and ticket count
+    MovieTicket(String movieName, int ticketCount) {
+        this(movieName, ticketCount, "Normal");
+    }
+
+    // Constructor with movie name, ticket count, and seat type
+    MovieTicket(String movieName, int ticketCount, String seatType) {
+        this.movieName = movieName;
+        this.ticketCount = ticketCount;
+        this.seatType = seatType;
+
+        if (seatType.equalsIgnoreCase("Normal")) {
+            this.pricePerTicket = 150;
+        } else if (seatType.equalsIgnoreCase("VIP")) {
+            this.pricePerTicket = 300;
+        } else {
+            this.pricePerTicket = 0;
+            System.out.println("Invalid seat type!");
+        }
+    }
+
+    void displayTicketDetails() {
+        System.out.println("Movie: " + movieName);
+        System.out.println("Tickets: " + ticketCount);
+        System.out.println("Seat Type: " + seatType);
+        System.out.println("Total Price: ₹" + (ticketCount * pricePerTicket));
+    }
+
+    public static void main(String[] args) {
+        MovieTicket booking1 = new MovieTicket("Inception", 2);
+        booking1.displayTicketDetails();
+
+        MovieTicket booking2 = new MovieTicket("Avatar", 3, "VIP");
+        booking2.displayTicketDetails();
+    }
+}
